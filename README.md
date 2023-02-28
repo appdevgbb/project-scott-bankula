f4c9f3919222137581fea7ed317485726fb8179d
Ray Kao <raykao@gmail.com>
2023-02-27 19:20:35 -0500

    add: initial firewall needs

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 
 [Azure DevOps - Azure Virtual Machine Scale Set agents](https://learn.microsoft.com/en-us/azure/devops/pipelines/agents/scale-set-agents?view=azure-devops)
 [Custom VM/VMSS Images with Packer](https://github.com/actions/runner-images/blob/main/images/linux/ubuntu2204.pkr.hcl)
+[Azure DevOps - Firewall Requirement](https://learn.microsoft.com/en-us/azure/devops/pipelines/agents/v2-linux?view=azure-devops#im-running-a-firewall-and-my-code-is-in-azure-repos-what-urls-does-the-agent-need-to-communicate-with)
--- infra/network/azurerm_virtual_network.tf
+++ infra/network/azurerm_virtual_network.tf
@@ -8,12 +8,23 @@ resource "azurerm_virtual_network" "default" {
   ]
 }
 
+
+# 16 x /28 Cidrs (10.0.0.0-15, 10.0.0.16-31, 10.0.0.32-47, etc)
+resource "azurerm_subnet" "firewall" {
+  name = "AzureFirewallSubnet"
+  resource_group_name = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes = [
+    cidrsubnet(azurerm_virtual_network.default.address_space[0], 12, 0)
+  ]
+}
+
 resource "azurerm_subnet" "apim" {
   name = "ApimSubnet"
   resource_group_name = azurerm_resource_group.default.name
   virtual_network_name = azurerm_virtual_network.default.name
   address_prefixes = [
-    cidrsubnet(azurerm_virtual_network.default.address_space[0], 8, 2)
+    cidrsubnet(azurerm_virtual_network.default.address_space[0], 12, 1)
   ]
 }
 
@@ -28,15 +39,25 @@ resource "azurerm_subnet" "aks_api" {
     }
   }
   address_prefixes = [
-    cidrsubnet(cidrsubnet(azurerm_virtual_network.default.address_space[0], 8, 3), 4, 0)
+    cidrsubnet(azurerm_virtual_network.default.address_space[0], 12, 2)
   ]
 }
 
+# /24 Cidrs starting from 10.0.4.0
 resource "azurerm_subnet" "aks_worker" {
   name = "AksWorkerSubnet"
   resource_group_name = azurerm_resource_group.default.name
   virtual_network_name = azurerm_virtual_network.default.name
   address_prefixes = [
     cidrsubnet(azurerm_virtual_network.default.address_space[0], 8, 4)
+  ]
+}
+
+resource "azurerm_subnet" "azdo" {
+  name = "AzdoSubnet"
+  resource_group_name = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes = [
+    cidrsubnet(azurerm_virtual_network.default.address_space[0], 8, 5)
   ]
 }
